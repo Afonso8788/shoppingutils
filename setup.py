cdbb7630d31086615f707317fe337313e3adb8ca
Afonso8788 <afonso.p.claro@ubi.pt>
2025-01-23 18:23:42 +0000

    a

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='shoppingutils',
     version='0.1',
-    packages=find_packages(),
     description='Utilities for managing shopping cart',
     author='Afonso Claro',
     author_email='afonso.p.claro@ubi.pt',
-    install_requires=[],
     url='https://github.com/Afonso8788/shoppingutils'
 )
