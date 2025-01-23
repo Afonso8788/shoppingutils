06ae0114731c2cdf7e5a20353a244c0d67bab4ad
Afonso8788 <afonso.p.claro@ubi.pt>
2025-01-23 20:24:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='shoppingutils',
-    version='0.1',
-    description='Utilities for managing shopping cart',
-    author='Afonso Claro',
-    author_email='afonso.p.claro@ubi.pt',
-    url='https://github.com/Afonso8788/shoppingutils'
+    name="shoppingutils",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A Python package for shopping cart utilities",
+    author="Afonso8788",
+    author_email="afonso.p.claro@ubi.pt",
+    url="https://github.com/Afonso8788/shoppingutils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
