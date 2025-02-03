9d7947a00e7c1cf771c8be90c02b7bbb13115762
Afonso8788 <afonso.p.claro@ubi.pt>
2025-02-03 16:10:30 +0000

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="shoppingutils",
+    version ="0.1",
+    description ="Shoppingutils Statement Package",
+    packages = find_packages(),
+    author = 'Afonso Claro',
+    author_email = 'afonso.p.claro@ubi.pt',
+    url = 'https://github.com/Afonso8788/shoppingutils')
+
+
+
