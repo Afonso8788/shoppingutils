131203fa777a1f6d28a7afbed931c338de58a789
Afonso8788 <afonso.p.claro@ubi.pt>
2025-02-03 16:01:18 +0000

    commit

--- shoppingutils/discount.py
+++ shoppingutils/discount.py
@@ -1,3 +1,3 @@
-def apply_discount(price, discount):
-    discounted_cart = [{'product': item['porduct'], 'price' : item['price']* (1-discount/100) for item in cart]
+def apply_discount(cart, discount):
+    discounted_cart = [{'product': item['product'], 'price' : item['price'] * (1-discount/100)} for item in cart]
     return discounted_cart
